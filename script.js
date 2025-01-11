b9df7726186ffc148b7c3d6bc0880df3856e24a6
psorapeter <psorapeter@gmail.com>
2025-01-11 17:01:28 +0100

    V3.0

--- script.js
+++ script.js
@@ -19,3 +19,18 @@
                 document.getElementById('brekileiras').innerText = data;
             })
             .catch(error => console.error('Error loading the file:', error));
+
+        fetch('usanka.txt')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('usankaleiras').innerText = data;
+            })
+            .catch(error => console.error('Error loading the file:', error));
+
+        fetch('utikalauz.txt')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('utikalauz').innerText = data;
+            })
+            .catch(error => console.error('Error loading the file:', error));
+            
