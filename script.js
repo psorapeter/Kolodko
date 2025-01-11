be189ce6d56a8fe04226d61ae717b3ef3eec9a9e
psorapeter <psorapeter@gmail.com>
2025-01-11 12:46:33 +0100

    V2

---
+++ script.js
@@ -0,0 +1,21 @@
+        // JavaScript kód a .txt fájl betöltéséhez
+        fetch('eletrajz.txt')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('eletrajz').innerText = data;
+            })
+            .catch(error => console.error('Error loading the file:', error));
+
+        fetch('balta.txt')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('baltaleiras').innerText = data;
+            })
+            .catch(error => console.error('Error loading the file:', error));
+
+        fetch('breki.txt')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('brekileiras').innerText = data;
+            })
+            .catch(error => console.error('Error loading the file:', error));
